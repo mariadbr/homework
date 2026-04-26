acae7bbc63e67934399b4d0de09516e47b70eab2
mariadbr <mdobrinevskaya@gmail.com>
2026-04-26 21:07:31 +0300

    Add files via upload

---
+++ practicetry_12/DebugTask6.java
@@ -0,0 +1,14 @@
+package practicetry_12;
+
+public class DebugTask6 {
+    public static void main(String[] args) {
+        countdown(5);
+    }
+
+    public static void countdown(int n) {
+        while(n >= 0 ) {
+            System.out.println(n);
+            n--;
+        }
+    }
+}
